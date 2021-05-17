54e5037b8ce38586edb57c65b07a3f6dc54a8421
AdolfGitHuber <74886323+AdolfGitHuber@users.noreply.github.com>
2021-05-17 19:52:53 +0800

    Update api.py

--- api.py
+++ api.py
@@ -35,7 +35,7 @@ class Email:
         return request
 
     @staticmethod
-    def fetching_message(email: str, message_id: str):
+    def fetching_message(email: str, message_id: int):
         """
         :param email: Email address
         :param message_id: message_id. Takes from func check_mailbox
@@ -50,11 +50,11 @@ class Email:
             htmlBody	Message body (html)
         """
         email = get_login(email)
-        request = requests.get(f'https://www.1secmail.com/api/v1/?action=readMessage&login={email[0]}&domain={email[1]}&id={message_id}').json()
+        request = requests.get(f'https://www.1secmail.com/api/v1/?action=readMessage&login={email[0]}&domain={email[1]}&id={str(message_id)}').json()
         return request
 
     @staticmethod
-    def attachment_download(email: str, message_id: str, file: str):
+    def attachment_download(email: str, message_id: int, file: str):
         """
         :param email: Email address
         :param message_id: message_id. Takes from func check_mailbox
@@ -62,7 +62,7 @@ class Email:
         :return: file
         """
         email = get_login(email)
-        request = requests.get(f'https://www.1secmail.com/api/v1/?action=download&login={email[0]}&domain={email[1]}&id={message_id}&file={file}')
+        request = requests.get(f'https://www.1secmail.com/api/v1/?action=download&login={email[0]}&domain={email[1]}&id={str(message_id)}&file={file}')
         return request
 
     @staticmethod
@@ -79,6 +79,7 @@ class Email:
             textBody	Message body (text)
             htmlBody	Message body (html)
         """
-        while not Email.check_mailbox(email):
+        data = len(Email.check_mailbox(email))
+        while len(Email.check_mailbox(email)) == data:
             sleep(1)
-        return Email.fetching_message(email, Email.check_mailbox(email)[0]['id'])
+        return Email.fetching_message(email, Email.check_mailbox(email)[-1]['id'])
